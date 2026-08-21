5f92f84a7b989446b53873ab3fa4ea7bbfded2f0
TheLostNexus5 <thelostnexus5@gmail.com>
2026-08-21 07:50:59 -0400

    Progress #2

--- js/main-page-js.js
+++ js/main-page-js.js
@@ -1,5 +1,8 @@
- document.addEventListener('scroll', function() {
-    const parallax = document.getElementById("parallaxImage");
+const parallax = document.getElementById("parallaxImage");
+const expandContainer = document.getElementById("container-1");
+const expandContainer2 = document.getElementById("container-2");
+
+document.addEventListener('scroll', function() {
     let scrollPosition = window.pageYOffset;
     
     /* 
@@ -8,4 +11,14 @@
     0.2 = Background moves very slowly.
     */
     parallax.style.transform = "translateY(" + (scrollPosition * 0.5) + "px)";
+    // 3. Get the current vertical scroll position
+    const scrollValue = window.scrollY;
+
+    // 4. Calculate the new size (Base size of 100px + scroll amount)
+
+    // 5. Apply the new size to the box
+    expandContainer.style.width = (1100 - scrollValue) + 'px';
+    
+    expandContainer2.style.width = (scrollValue-200) + 'px';
+
 });
