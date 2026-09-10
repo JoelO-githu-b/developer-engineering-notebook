8b714d2510de89d230dd9d5cf0223323d0d0392d
TheLostNexus5 <thelostnexus5@gmail.com>
2026-09-10 12:57:11 -0400

    Added table css that changes to the light/dark theme
    yeah

--- js/weekly-journal-js.js
+++ js/weekly-journal-js.js
@@ -5,8 +5,12 @@ const wave_wrapper = document.getElementById("wave-wrapper");
 const wave_wrapper2 = document.getElementById("wave-wrapper-2");
 const wavy_banner = document.getElementById("wavy-banner");
 const banner_content = document.getElementById("banner-content");
+const tbl = document.getElementById("tbl")
 const currentTheme = localStorage.getItem('theme');
-
+const all_tables = document.querySelectorAll('table');
+const all_rows = document.querySelectorAll('tr');
+const all_headers = document.querySelectorAll('th');
+const all_cells = document.querySelectorAll('td');
 if (currentTheme === 'dark') {
   toggleDarkMode(true)
 }
@@ -19,6 +23,16 @@ wave_wrapper.classList.toggle("dark-wave-wrapper-2")
   wave_wrapper2.classList.toggle("dark-wave-wrapper")
   wavy_banner.classList.toggle("wavy-banner-dark")
   banner_content.classList.toggle("banner-content-light")
+  
+  all_tables.forEach(table => {
+      table.classList.toggle('table-dark-mode')
+  });
+  all_headers.forEach(table => {
+      table.classList.toggle('table-header-dark-mode')
+  });
+  all_cells.forEach(table => {
+      table.classList.toggle('table-cell-dark-mode')
+  });
   if (dontFlip) return;
   if (currentTheme === "dark") {
     localStorage.setItem('theme', 'light');
